5573396cd494fcb2f51d7557416a3e1ceaf6663c
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-15 20:52:09 +0530

    [sanjana] Add : Find frequency of each element

--- Practice_Problem_4B/Program.cs
+++ Practice_Problem_4B/Program.cs
@@ -6,42 +6,39 @@
         {
             int[] arr = { 1, 2, 1, 3, 5, 8, 7, 2, 9, 5, 3 };
             int n = arr.Length;
-            int j = 0, i = 0, k = 0, ctr = 0;
-
-            int[] b = new int[arr.Length];
-
-            Console.Write("\nThe unique elements found in the array are : \n");
+            int[] fr1 = new int[n] ;
+            for(int m=0;m<n; m++)
+            {
+                fr1[m] = -1;
+            }
+            
+            int ctr=0,i=0,j=0;
             for (i = 0; i < n; i++)
             {
-                ctr = 0;
-          
-                for (j = 0; j < i - 1; j++)
+                ctr = 1;
+                for (j = i + 1; j < n; j++)
                 {
-                   
                     if (arr[i] == arr[j])
                     {
                         ctr++;
+                        fr1[j] = 0;
                     }
                 }
-              
-                for (k = i + 1; k < n; k++)
+
+                if (fr1[i] != 0)
                 {
-                    
-                    if (arr[i] == arr[k])
-                    {
-                        ctr++;
-                    }
-               
-                    if (arr[i] == arr[i + 1])
-                    {
-                        i++;
-                    }
-                }
-                if (ctr == 0)
-                {
-                    Console.Write("{0} ", arr[i]);
+                    fr1[i] = ctr;
                 }
             }
+            Console.Write("\nThe frequency of all elements of the array : \n");
+            for (i = 0; i < n; i++)
+            {
+                if (fr1[i] != 0)
+                {
+                    Console.Write("{0} occurs {1} times\n", arr[i], fr1[i]);
+                }
+            }
+
         }
     }
 }
