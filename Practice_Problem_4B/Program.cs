2d0431672760c74d8029ed6f30485bd6be064170
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-15 19:06:04 +0530

    [sanjana] Add : to get count of duplicates in array elements

--- Practice_Problem_4B/Program.cs
+++ Practice_Problem_4B/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Practice_Problems_4B
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = {1,2,1,3,5,8,7,2,9,5,3 };
+            int count = 0;
+            for(int i=0;i<arr.Length; i++)
+            {
+                for(int j=i+1;j<arr.Length;j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine("Count for Duplicate elements => "+count);
+        }
+    }
+}
