a68e8fd27bf935de4eb1aaaed4aac25eef41b10e
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-15 20:12:34 +0530

    [sanjana] Add : Print Unique elements in array

--- Practice_Problem_4B/Program.cs
+++ Practice_Problem_4B/Program.cs
@@ -4,20 +4,44 @@
     {
         public static void Main(string[] args)
         {
-            int[] arr = {1,2,1,3,5,8,7,2,9,5,3 };
-            int count = 0;
-            for(int i=0;i<arr.Length; i++)
+            int[] arr = { 1, 2, 1, 3, 5, 8, 7, 2, 9, 5, 3 };
+            int n = arr.Length;
+            int j = 0, i = 0, k = 0, ctr = 0;
+
+            int[] b = new int[arr.Length];
+
+            Console.Write("\nThe unique elements found in the array are : \n");
+            for (i = 0; i < n; i++)
             {
-                for(int j=i+1;j<arr.Length;j++)
+                ctr = 0;
+          
+                for (j = 0; j < i - 1; j++)
                 {
+                   
                     if (arr[i] == arr[j])
                     {
-                        count++;
-                        break;
+                        ctr++;
                     }
                 }
+              
+                for (k = i + 1; k < n; k++)
+                {
+                    
+                    if (arr[i] == arr[k])
+                    {
+                        ctr++;
+                    }
+               
+                    if (arr[i] == arr[i + 1])
+                    {
+                        i++;
+                    }
+                }
+                if (ctr == 0)
+                {
+                    Console.Write("{0} ", arr[i]);
+                }
             }
-            Console.WriteLine("Count for Duplicate elements => "+count);
         }
     }
 }
